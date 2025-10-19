4a232636e7833e7d64c819b0e7decedef5ee3607
muradiali220 <muradiali220@gmail.com>
2025-10-19 14:39:56 +0400

    add new python file

---
+++ lis append.py
@@ -0,0 +1,30 @@
+"""
+
+value=[1,16,9,4]
+value.sort(reverse=True)
+print(value)
+
+"""
+"""
+ lis=[1,2,3]
+newlis=lis*100
+print(newlis)
+"""
+
+
+
+
+"""
+lis=[1,2,3,4]
+newlis=list(lis)#we make  new list not same lis befor
+lis.append(5)
+newlis.append(6)
+print(newlis)
+
+"""
+
+
+lis=[1,2,3,4,5,6]
+newlis=lis[2:5]
+print(newlis)
+
